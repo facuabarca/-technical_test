3b269363cc358ef652d12994597a2ac531a73ab7
Facundo Abarca Aparicio <facundo.abarca@bcn.grupoica.com>
2019-03-14 19:27:45 +0100

    deleting innecesary assets and change functions name

--- src/app/login/pages/register/register.service.ts
+++ src/app/login/pages/register/register.service.ts
@@ -16,7 +16,7 @@ export class RegisterService {
         private router: Router
     ) {}
     
-    sonIguales(campo1: string, campo2: string) {
+    passwordEqual(campo1: string, campo2: string) {
         return( group: FormGroup) => {
           const pass1 = group.controls[campo1].value;
           const pass2 = group.controls[campo2].value;
@@ -24,7 +24,7 @@ export class RegisterService {
             return null;
           }
           return  {
-            sonIguales: true
+            passwordEqual: true
           };
         };
     }
@@ -35,7 +35,7 @@ export class RegisterService {
             email: new FormControl(null, [Validators.required, Validators.email]),
             password: new FormControl(null, Validators.required),
             password2: new FormControl(null, Validators.required),
-          }, { validators: this.sonIguales('password', 'password2') });
+          }, { validators: this.passwordEqual('password', 'password2') });
 		return formGroup;
     }
     
