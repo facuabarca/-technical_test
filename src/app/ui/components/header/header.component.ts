b91703e6c64da631301e7e6b62bcfe8f04d83412
Facundo Abarca Aparicio <facundo.abarca@bcn.grupoica.com>
2019-03-14 16:32:29 +0100

    navbar styles, center logo

--- src/app/ui/components/header/header.component.ts
+++ src/app/ui/components/header/header.component.ts
@@ -1,16 +1,19 @@
 import { Component } from "@angular/core";
 import { CacheService } from 'src/app/core/services';
+import { Router } from '@angular/router';
 
 @Component({
   selector: "app-header",
-  templateUrl: "./header.component.html"
+  templateUrl: "./header.component.html",
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
 
-  constructor(private cacheService: CacheService) {
+  constructor(private cacheService: CacheService, private router: Router) {
 
   }
   logout(): void {
     this.cacheService.clear('userAuth');
+    this.router.navigate(['login']);
   }
 }
